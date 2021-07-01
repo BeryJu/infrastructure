bc1a9df7cec2b8660bb7e3fa3b13e7f71bdcae00
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-01 16:21:52 +0200

    tf: update rancher

--- tf/rancher/main.tf
+++ tf/rancher/main.tf
@@ -19,12 +19,59 @@ provider "rancher2" {
 
 resource "rancher2_cluster" "beryjuorg-prd" {
   name = "beryjuorg-prd"
+  cluster_monitoring_input {
+    answers = {
+      "exporter-kubelets.https"                   = "true"
+      "exporter-node.enabled"                     = "true"
+      "exporter-node.ports.metrics.port"          = "9796"
+      "exporter-node.resources.limits.cpu"        = "200m"
+      "exporter-node.resources.limits.memory"     = "200Mi"
+      "grafana.persistence.enabled"               = "false"
+      "grafana.persistence.size"                  = "10Gi"
+      "grafana.persistence.storageClass"          = "default"
+      "operator-init.enabled"                     = "true"
+      "operator.resources.limits.memory"          = "500Mi"
+      "prometheus.persistence.enabled"            = "false"
+      "prometheus.persistence.size"               = "50Gi"
+      "prometheus.persistence.storageClass"       = "default"
+      "prometheus.persistent.useReleaseName"      = "true"
+      "prometheus.resources.core.limits.cpu"      = "1000m"
+      "prometheus.resources.core.limits.memory"   = "1000Mi"
+      "prometheus.resources.core.requests.cpu"    = "750m"
+      "prometheus.resources.core.requests.memory" = "750Mi"
+      "prometheus.retention"                      = "12h"
+    }
+  }
 }
 
 resource "rancher2_cluster" "beryjuorg-backup" {
   name = "beryjuorg-backup"
   k3s_config {
     version = "v1.19.4+k3s1"
+  }
+
+  cluster_monitoring_input {
+    answers = {
+      "exporter-kubelets.https"                   = "true"
+      "exporter-node.enabled"                     = "true"
+      "exporter-node.ports.metrics.port"          = "9796"
+      "exporter-node.resources.limits.cpu"        = "200m"
+      "exporter-node.resources.limits.memory"     = "200Mi"
+      "grafana.persistence.enabled"               = "false"
+      "grafana.persistence.size"                  = "10Gi"
+      "grafana.persistence.storageClass"          = "default"
+      "operator-init.enabled"                     = "true"
+      "operator.resources.limits.memory"          = "500Mi"
+      "prometheus.persistence.enabled"            = "false"
+      "prometheus.persistence.size"               = "50Gi"
+      "prometheus.persistence.storageClass"       = "default"
+      "prometheus.persistent.useReleaseName"      = "true"
+      "prometheus.resources.core.limits.cpu"      = "200m"
+      "prometheus.resources.core.limits.memory"   = "750Mi"
+      "prometheus.resources.core.requests.cpu"    = "150m"
+      "prometheus.resources.core.requests.memory" = "500Mi"
+      "prometheus.retention"                      = "12h"
+    }
   }
 }
 
