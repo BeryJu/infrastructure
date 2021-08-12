2f9e001cb9ad7a931225a5102e134e4385cd6083
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-12 19:47:18 +0200

    tf: add map_public_ip_on_launch

--- tf/aws/main.tf
+++ tf/aws/main.tf
@@ -10,13 +10,16 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.121.0.0/16"
+  tags = {
+    Name = "beryjuorg-default"
+  }
 }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.121.0.0/24"
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.121.0.0/24"
+  map_public_ip_on_launch = true
   tags = {
-    Name = "Default"
+    Name = "beryjuorg-default"
   }
 }
