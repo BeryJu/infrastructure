e8956a2a136229b2fbda3758e96873328a58476a
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-26 21:02:59 +0200

    tf: add ldap flow

---
+++ tf/authentik/flow-ldap.tf
@@ -0,0 +1,34 @@
+resource "authentik_stage_password" "ldap-flow-password" {
+  name     = "ldap-flow-password"
+  backends = ["authentik.core.auth.InbuiltBackend", "authentik.core.auth.TokenBackend"]
+}
+
+resource "authentik_stage_identification" "ldap-flow-identification" {
+  name           = "ldap-flow-identification"
+  user_fields    = ["username"]
+  sources        = []
+  password_stage = authentik_stage_password.ldap-flow-password.id
+}
+
+resource "authentik_stage_user_login" "ldap-flow-login" {
+  name = "ldap-flow-login"
+}
+
+resource "authentik_flow" "ldap-flow" {
+  name        = "LDAP"
+  title       = "LDAP"
+  slug        = "ldap-flow"
+  designation = "authorization"
+}
+
+resource "authentik_flow_stage_binding" "ldap-identification-binding" {
+  target = authentik_flow.ldap-flow.uuid
+  stage  = authentik_stage_identification.ldap-flow-identification.id
+  order  = 10
+}
+
+resource "authentik_flow_stage_binding" "ldap-login-bind" {
+  target = authentik_flow.ldap-flow.uuid
+  stage  = authentik_stage_user_login.ldap-flow-login.id
+  order  = 20
+}
