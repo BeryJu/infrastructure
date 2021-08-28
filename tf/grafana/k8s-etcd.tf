61067bec268e9a3a184fef18c9f51db42b02b840
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-28 12:42:19 +0200

    tf: remove etcd since its not in pods

--- tf/grafana/k8s-etcd.tf
+++
@@ -1,9 +0,0 @@
-data "http" "k8s-etcd-dashboard" {
-  url = "https://raw.githubusercontent.com/etcd-io/website/master/content/en/docs/v3.4/op-guide/grafana.json"
-}
-
-resource "grafana_dashboard" "k8s-etcd" {
-  folder      = grafana_folder.beryjuorg-k8s-preset.id
-  # For some reason that json file is a dashboard wrapped in { "etcd.json": {}  }
-  config_json = jsonencode(jsondecode(data.http.k8s-etcd-dashboard.body)["etcd.json"])
-}
