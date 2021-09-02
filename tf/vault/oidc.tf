0e0d54641eaf4fbc8e67881f5c41e0daca310706
Jens Langhammer <jens.langhammer@beryju.org>
2021-09-02 20:35:55 +0200

    managed vault sso

---
+++ tf/vault/oidc.tf
@@ -0,0 +1,35 @@
+data "vault_generic_secret" "vault_oidc" {
+  path = "kv/services/vault"
+}
+
+resource "vault_jwt_auth_backend" "authentik-oidc" {
+  path               = "authentik"
+  default_role       = "default_role"
+  type               = "oidc"
+  oidc_discovery_url = "https://id.beryju.org/application/o/hashicorp-vault/"
+  oidc_client_id     = "413c74e97eb0002cb1ecb2056ce5844af7c1b63d"
+  oidc_client_secret = data.vault_generic_secret.vault_oidc.data["oauth_secret"]
+  bound_issuer       = "https://id.beryju.org/application/o/hashicorp-vault/"
+  tune {
+    default_lease_ttl  = "24h"
+    listing_visibility = "unauth"
+    max_lease_ttl      = "24h"
+    token_type         = "default-service"
+  }
+}
+
+resource "vault_jwt_auth_backend_role" "default_role" {
+  backend   = vault_jwt_auth_backend.authentik-oidc.path
+  role_name = "default_role"
+  role_type = "oidc"
+  allowed_redirect_uris = [
+    "http://localhost:8250/oidc/callback",
+    "https://vault.beryju.org/ui/vault/auth/authentik/oidc/callback"
+  ]
+  bound_claims_type    = "string"
+  groups_claim         = "groups"
+  oidc_scopes          = ["openid", "profile", "email"]
+  token_policies       = ["admin"]
+  user_claim           = "email"
+  verbose_oidc_logging = true
+}
