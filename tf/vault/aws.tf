92036a9605d18e6bc4313529db8b7ace6d6d39b9
Jens Langhammer <jens.langhammer@beryju.org>
2021-09-03 14:40:57 +0200

    tf: get terraform aws creds via vault

--- tf/vault/aws.tf
+++ tf/vault/aws.tf
@@ -13,3 +13,11 @@ resource "vault_aws_secret_backend_role" "role-ses" {
 
   policy_document = file("./vault/policies/ses-send.json")
 }
+
+resource "vault_aws_secret_backend_role" "role-admin" {
+  backend = "aws"
+  name    = "admin"
+  credential_type = "iam_user"
+
+  policy_document = file("./vault/policies/admin.json")
+}
