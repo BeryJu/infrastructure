8dd1a5bd7009ae17e634006e2e3c412b80403b12
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-17 15:01:34 +0200

    yeah

--- tf/artifactory/main.tf
+++ tf/artifactory/main.tf
@@ -13,7 +13,7 @@ data "vault_generic_secret" "artifactory_auth" {
 
 provider "artifactory" {
   url      = "https://registry.beryju.org/artifactory"
-  api_key = data.vault_generic_secret.artifactory_auth.data["api_key"]
+  access_token = data.vault_generic_secret.artifactory_auth.data["api_key"]
 }
 
 resource "artifactory_local_repository" "docker-priv" {
--- tf/vault/monitoring.tf
+++ tf/vault/monitoring.tf
@@ -7,22 +7,3 @@ path "sys/metrics" {
 }
 EOT
 }
-
-resource "vault_token" "monitoring-token" {
-  role_name = "app"
-
-  policies = [vault_policy.monitoring.name]
-
-  renewable = true
-  ttl = "30d"
-}
-
-resource "vault_generic_secret" "monitoring-token-secret" {
-  path = "kv/services/prometheus/vault"
-
-  data_json = <<EOT
-{
-  "token": "${vault_token.monitoring-token.client_token}",
-}
-EOT
-}
