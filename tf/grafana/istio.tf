63cfdff094f098350897502d3169f44b4aa395b6
Jens Langhammer <jens.langhammer@beryju.org>
2022-01-09 19:59:31 +0100

    add istio dashboards

---
+++ tf/grafana/istio.tf
@@ -0,0 +1,57 @@
+resource "grafana_folder" "beryjuorg-istio" {
+  title = "BeryJu.org Istio"
+}
+
+data "http" "istio-extension-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/istio-extension-dashboard.json"
+}
+
+resource "grafana_dashboard" "istio-extension-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.istio-extension-dashboard.body
+}
+
+data "http" "istio-mesh-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/istio-mesh-dashboard.json"
+}
+
+resource "grafana_dashboard" "istio-mesh-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.istio-mesh-dashboard.body
+}
+
+data "http" "istio-performance-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/istio-performance-dashboard.json"
+}
+
+resource "grafana_dashboard" "istio-performance-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.istio-performance-dashboard.body
+}
+
+data "http" "istio-service-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/istio-service-dashboard.json"
+}
+
+resource "grafana_dashboard" "istio-service-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.istio-service-dashboard.body
+}
+
+data "http" "istio-workload-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/istio-workload-dashboard.json"
+}
+
+resource "grafana_dashboard" "istio-workload-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.istio-workload-dashboard.body
+}
+
+data "http" "pilot-dashboard" {
+  url = "https://raw.githubusercontent.com/istio/istio/master/manifests/addons/dashboards/pilot-dashboard.json"
+}
+
+resource "grafana_dashboard" "pilot-dashboard" {
+  folder      = grafana_folder.beryjuorg-istio.id
+  config_json = data.http.pilot-dashboard.body
+}
