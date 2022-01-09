b01c30beff3dc7c0ec183618b5236d3da6df7a7d
Jens Langhammer <jens.langhammer@beryju.org>
2022-01-09 12:57:51 +0100

    add istio pki

---
+++ tf/vault/pki-istio.tf
@@ -0,0 +1,35 @@
+resource "vault_mount" "pki-istio" {
+  path        = "pki-istio"
+  type        = "pki"
+  description = "istio PKI"
+
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 315360000
+}
+
+resource "vault_pki_secret_backend_config_urls" "pki-istio-urls" {
+  backend                 = vault_mount.pki-istio.path
+  issuing_certificates    = ["https://vault.beryju.org/v1/pki-istio/ca"]
+  crl_distribution_points = ["https://vault.beryju.org/v1/pki-istio/crl"]
+}
+
+resource "vault_pki_secret_backend_role" "pki-istio-role" {
+  backend           = vault_mount.pki-istio.path
+  name              = "beryjuorg-istio"
+  ttl               = 90 * 24 * 60 * 60
+  allow_ip_sans     = true
+  key_type          = "rsa"
+  key_bits          = 4096
+  allow_any_name    = true
+  enforce_hostnames = false
+  require_cn        = false
+  allowed_uri_sans  = ["spiffe://*"]
+  allowed_domains   = ["istio-ca"]
+}
+
+resource "vault_pki_secret_backend_cert" "istio-root" {
+  backend = vault_mount.pki-istio.path
+  name    = "internal"
+
+  common_name = "istio-ca-vault"
+}
