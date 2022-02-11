dc8054e2ed114b0e2c45b281ec88193efcf01dfe
Jens Langhammer <jens.langhammer@beryju.org>
2022-02-11 20:00:44 +0100

    tf: add puppet cert auth to vault

---
+++ tf/vault/puppet.tf
@@ -0,0 +1,25 @@
+resource "vault_auth_backend" "puppet-cert" {
+  type = "cert"
+  path = "puppet-cert"
+}
+
+resource "vault_cert_auth_backend_role" "puppet-root" {
+  name        = "puppet"
+  certificate = file("./files/puppet.pem")
+  backend     = vault_auth_backend.puppet-cert.path
+  ttl         = 3600
+  policies    = ["puppet-node"]
+}
+
+resource "vault_policy" "puppet-node" {
+  name = "puppet-node"
+
+  policy = <<EOT
+path "kv/data/*" {
+  capabilities = ["read"]
+}
+path "aws/creds/*" {
+  capabilities = ["read"]
+}
+EOT
+}
