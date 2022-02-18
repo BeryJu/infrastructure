000d673eb6a74319ef918a07a78e71830728767b
Jens Langhammer <jens.langhammer@beryju.org>
2022-02-18 11:13:22 +0000

    migrate more

--- tf/aws_route53/beryju.org-infra.tf
+++ tf/aws_route53/beryju.org-infra.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "registry-beryju-org" {
   name    = "registry.beryju.org"
   type    = "A"
   alias {
-    name                   = "k8s-prd._aliases.beryju.org"
+    name                   = "k8s-prd-istio._aliases.beryju.org."
     zone_id                = aws_route53_zone.beryju-org.zone_id
     evaluate_target_health = true
   }
@@ -24,7 +24,7 @@ resource "aws_route53_record" "id-beryju-org" {
   name    = "id.beryju.org"
   type    = "A"
   alias {
-    name                   = "k8s-prd._aliases.beryju.org"
+    name                   = "k8s-prd-istio._aliases.beryju.org."
     zone_id                = aws_route53_zone.beryju-org.zone_id
     evaluate_target_health = true
   }
