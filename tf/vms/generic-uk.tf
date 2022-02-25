50d36b5d2f5821e5c4878bb5589cb6642330a179
Jens Langhammer <jens.langhammer@beryju.org>
2022-02-25 10:48:53 +0100

    more vms

--- tf/vms/generic-uk.tf
+++ tf/vms/generic-uk.tf
@@ -50,3 +50,35 @@ module "mc" {
     ]
   }
 }
+
+module "ak-dev-dc" {
+  source = "../modules/vsphere_vm"
+  name   = "ak-dev-dc1.lab.beryju.org"
+  vsphere = {
+    resource_pool = data.vsphere_resource_pool.uk-heavy.id
+    datastore     = data.vsphere_datastore_cluster.ssd-london.id
+    network       = data.vsphere_network.uk-vs-int-101-srv.id
+  }
+
+  spec = {
+    cpu       = 2
+    memory    = 4096
+    disk_size = 20
+  }
+}
+
+module "ak-dev-k3s" {
+  source = "../modules/vsphere_vm"
+  name   = "ak-dev-k3s1.lab.beryju.org"
+  vsphere = {
+    resource_pool = data.vsphere_resource_pool.uk-heavy.id
+    datastore     = data.vsphere_datastore_cluster.ssd-london.id
+    network       = data.vsphere_network.uk-vs-int-101-srv.id
+  }
+
+  spec = {
+    cpu       = 2
+    memory    = 6144
+    disk_size = 20
+  }
+}
