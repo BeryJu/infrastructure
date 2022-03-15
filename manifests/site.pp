9d1e6acf7107cc784658d8c8ef3351e061252739
Jens Langhammer <jens.langhammer@beryju.org>
2022-03-15 16:19:04 +0000

    dont load classes for windows for now

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,9 @@
-lookup('classes', {merge => unique}).include
 node /puppet.*/ {
-  # Configure puppetdb and its underlying database
   class { 'puppetdb': }
-
-  # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
 }
 node default {
-  lookup('classes', {merge => unique}).include
+  if $::facts[os][family] != "windows" {
+    lookup('classes', {merge => unique}).include
+  }
 }
