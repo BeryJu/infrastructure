473170dddcdd09a5bbfc1443aa69541e206c2ed3
Jens Langhammer <jens.langhammer@beryju.org>
2022-03-15 17:19:41 +0000

    shorten cloudinit fact

--- modules/beryjuorg_common/lib/facter/vmware_guestinfo.rb
+++ modules/beryjuorg_common/lib/facter/vmware_guestinfo.rb
@@ -4,14 +4,14 @@ require 'facter'
 require 'yaml'
 require 'base64'
 
-Facter.add(:vmware_guestinfo_userdata) do
+Facter.add(:cloudinit_userdata) do
   setcode do
     userdata = Base64.decode64(`vmware-rpctool "info-get guestinfo.userdata"`)
     YAML.load(userdata)
   end
 end
 
-Facter.add(:vmware_guestinfo_metadata) do
+Facter.add(:cloudinit_metadata) do
   setcode do
     metadaat = Base64.decode64(`vmware-rpctool "info-get guestinfo.metadata"`)
     YAML.load(metadaat)
