d88485ec48874ba561faa3dcce56a14de1e1290c
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-03 17:03:59 +0200

    hmm

--- tf/vault/github.tf
+++ tf/vault/github.tf
@@ -1,19 +1,3 @@
-resource "vault_policy" "github-terraform" {
-  name = "github-terraform"
-
-  policy = <<EOT
-path "kv/data/*" {
-  capabilities = ["read"]
-}
-path "aws/creds/*" {
-  capabilities = ["read"]
-}
-path "*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
-}
-EOT
-}
-
 module "github-vault-oidc" {
   source  = "digitalocean/github-oidc/vault"
   version = "~> 1.0.0"
@@ -24,7 +8,7 @@ module "github-vault-oidc" {
       vault_role_name : "github-terraform",
       bound_subject : "repo:BeryJu/infrastructure:ref:refs/heads/master",
       vault_policies : [
-        "github-terraform"
+        "admin"
       ],
     },
   ]
