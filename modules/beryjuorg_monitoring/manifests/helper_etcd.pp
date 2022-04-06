daa5cf940aa4969a83cdacfa6c514de8590c2b46
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-06 10:14:52 +0000

    fix

--- modules/beryjuorg_monitoring/manifests/helper_etcd.pp
+++ modules/beryjuorg_monitoring/manifests/helper_etcd.pp
@@ -1,9 +1,7 @@
 class beryjuorg_monitoring::helper_etcd {
 
-  $host = regsubst($fqdn, '\.', '-', 'G')
-
   cron { 'etcd_export':
-    command => "bash -c \"curl --cacert /etc/kubernetes/ssl/kube-ca.pem --cert /etc/kubernetes/ssl/kube-etcd-${host}.pem --key /etc/kubernetes/ssl/kube-etcd-${host}-key.pem https://localhost:2379/metrics > /var/lib/prometheus/node-exporter/etcd.prom\"",
+    command => "bash -c \"curl -s --cacert /etc/kubernetes/ssl/kube-ca.pem --cert $(find /etc/kubernetes/ssl/ -name 'kube-etcd-*' | grep -v key) --key $(find /etc/kubernetes/ssl/ -name 'kube-etcd-*key.pem') https://localhost:2379/metrics > /var/lib/prometheus/node-exporter/etcd.prom\"",
     user    => 'root',
     hour    => "*",
     minute  => "*",
--- modules/beryjuorg_monitoring/manifests/helper_local_prom.pp
+++ modules/beryjuorg_monitoring/manifests/helper_local_prom.pp
@@ -1,7 +1,5 @@
 class beryjuorg_monitoring::helper_local_prom {
 
-  $host = regsubst($fqdn, '\.', '-', 'G')
-
   cron { 'etcd_export':
     command => 'curl "http://localhost:9090/federate?match[]=\{__name__=~\".*\"\}" > /var/lib/prometheus/node-exporter/prometheus-local.prom',
     user    => 'root',
