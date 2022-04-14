8f64bae9f85ff3c3e16490428039b3c0548a640a
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-14 09:39:29 +0000

    fix again

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
 node /puppet.*/ {
-  class { 'puppetdb': }
-  class { 'puppetdb::master::config':
-    listen_addresses => $facts['ipaddress'],
+  class { 'puppetdb':
+    listen_address => $facts['ipaddress'],
   }
+  class { 'puppetdb::master::config': }
   lookup('classes', {merge => unique}).include
 }
 node default {
