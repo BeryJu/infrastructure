b742d76cdfb584f57ae3ddc6194c23fffc8ba8c8
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-18 17:44:12 +0200

    stuff

--- scratch/down.sh
+++ scratch/down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 source ./common.sh
 
-ssh puppet1.prod.beryju.org -- sudo /opt/puppetlabs/bin/puppetserver ca clean --certname scratch-vm.lab.beryju.org
+ssh puppet1.prod.beryju.org -- sudo /opt/puppetlabs/bin/puppetserver ca clean --certname scratch-vm.lab.beryju.org || true
 
 doctl compute droplet delete $VM_NAME
--- scratch/up.sh
+++ scratch/up.sh
@@ -10,6 +10,7 @@ doctl compute droplet create $VM_NAME \
     --image ubuntu-20-04-x64 \
     --ssh-keys "$ssh_keys" \
     --user-data-file "$SCRIPT_DIR/userdata.sh" \
-    --wait
+    --wait \
+    --tag-name scratch-vm
 
 ssh puppet1.prod.beryju.org -- sudo /opt/puppetlabs/bin/puppetserver ca sign --certname scratch-vm.lab.beryju.org
