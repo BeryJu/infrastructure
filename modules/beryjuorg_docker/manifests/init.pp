c7f1bfdb050d2e65cfd85747e08350f37a1d9982
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-30 19:38:17 +0200

    use cadvisor

--- modules/beryjuorg_docker/manifests/init.pp
+++ modules/beryjuorg_docker/manifests/init.pp
@@ -15,9 +15,25 @@ class beryjuorg_docker {
     registry_mirror  => [
       'https://proxy.registry.beryju.org'
     ],
-    extra_parameters => '--live-restore'
+    extra_parameters => '--live-restore --metrics-addr localhost:9101'
   }
   ->sysctl { 'vm.swappiness': value => '0' }
+
+  docker::run { 'cadvisor':
+    image            => 'gcr.io/cadvisor/cadvisor',
+    detach           => true,
+    ports            => ['127.0.0.1:8080:8080'],
+    volumes          => [
+      '/:/rootfs:ro',
+      '/var/run:/var/run:ro',
+      '/sys:/sys:ro',
+      '/var/lib/docker/:/var/lib/docker:ro',
+      '/dev/disk/:/dev/disk:ro',
+    ],
+    privileged       => true,
+    extra_parameters => [ '--restart=always', '--device=/dev/kmsg' ],
+  }
+  include beryjuorg_monitoring::helper_cadvisor
 
   $metrics = $facts['docker']['network'].map |$name, $network| {
     beryjuorg_monitoring::metric { "docker-network-${name}":
