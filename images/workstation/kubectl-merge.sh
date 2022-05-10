69131a2c0aa1ef68ddd3bb642b26bdc32502fc09
Jens Langhammer <jens.langhammer@beryju.org>
2022-05-10 12:42:28 +0000

    cleanup

--- images/workstation/kubectl-merge.sh
+++ images/workstation/kubectl-merge.sh
@@ -1,18 +1,22 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 cd ~/k8s
 export KUBECONFIG=""
 
+echo "Getting rancher clusters..."
 # Get all clusters from rancher
 clusters=$(rancher cluster ls --format '{{.Cluster.Name}}')
 for cluster in $clusters; do
+    echo -e "\t Getting kubeconfig for cluster $cluster..."
     rancher cluster kf $cluster > "${cluster}.yaml"
 done
 
+echo "Getting other kubeconfig files..."
 # Load all yml and yaml files as kubeconfig files
 for config in *y*ml; do
+    echo -e "\t Adding kubeconfig for $config..."
     export KUBECONFIG=$KUBECONFIG:$config
 done
 
 # Merge them all into a single kubeconfig
 kubectl config view --flatten > ~/.kube/config
-unexport KUBECONFIG
+unset KUBECONFIG
