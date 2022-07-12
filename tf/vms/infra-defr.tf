f24058aa175513a0dd33b90808fb9923379c7681
Jens Langhammer <jens.langhammer@beryju.org>
2022-07-12 17:10:24 +0000

    add thigns

--- tf/vms/infra-defr.tf
+++ tf/vms/infra-defr.tf
@@ -31,3 +31,20 @@ module "maas-defr" {
     disk_size = 15
   }
 }
+
+module "gravity-defr" {
+  source = "../modules/vsphere_vm"
+  name   = "gravity-defr1.prod.beryju.org"
+  vsphere = {
+    resource_pool = data.vsphere_resource_pool.de-general.id
+    datastore     = data.vsphere_datastore_cluster.ssd-freiburg.id
+    network       = data.vsphere_network.de-lan.id
+    datacenter    = data.vsphere_datacenter.de-freiburg.id
+  }
+
+  spec = {
+    cpu       = 1
+    memory    = 1024
+    disk_size = 10
+  }
+}
