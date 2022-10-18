208ebdb329c1da51c2cdb36cc957322fffc879f5
Jens Langhammer <jens.langhammer@beryju.org>
2022-10-18 22:21:25 +0200

    update

--- tf/aws/route53/main.tf
+++ tf/aws/route53/main.tf
@@ -5,14 +5,6 @@ locals {
   pub_k8s_istio_ip = "194.56.224.182"
 }
 
-resource "aws_route53_record" "k8s-istio" {
-  zone_id = aws_route53_zone.beryju-org.zone_id
-  name    = "k8s-prd-istio._aliases.beryju.org"
-  type    = "A"
-  ttl     = "300"
-  records = [local.pub_k8s_istio_ip]
-}
-
 resource "aws_route53_record" "k8s-prd" {
   zone_id = aws_route53_zone.beryju-org.zone_id
   name    = "k8s-prd._aliases.beryju.org"
