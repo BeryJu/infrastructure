ff056d56bdcda986a233be21bb97f405c6d11cdb
Jens Langhammer <jens.langhammer@beryju.org>
2022-10-20 16:42:55 +0200

    dont use wildcard

--- tf/aws/route53/goauthentik.io.tf
+++ tf/aws/route53/goauthentik.io.tf
@@ -7,13 +7,34 @@ resource "aws_route53_record" "goauthentik-main" {
     local.pub_netlify_ip,
   ]
 }
-resource "aws_route53_record" "goauthentik-wildcard" {
+
+locals {
+  versions = [
+    "version-0-13",
+    "version-0-14",
+    "version-2021-1",
+    "version-2021-2",
+    "version-2021-3",
+    "version-2022-2",
+    "version-2022-3",
+    "version-2022-5",
+    "version-2022-6",
+    "version-2022-7",
+    "version-2022-8",
+    "version-2022-9",
+    "version-2022-10",
+  ]
+}
+
+resource "aws_route53_record" "goauthentik-version-branch" {
   zone_id = aws_route53_zone.goauthentik-io.zone_id
-  name    = "*.goauthentik.io"
+  name    = "${each.key}.goauthentik.io"
   type    = "CNAME"
   ttl     = "300"
   records = ["authentik.netlify.app."]
+  for_each = toset(local.versions)
 }
+
 resource "aws_route53_record" "goauthentik-version" {
   zone_id = aws_route53_zone.goauthentik-io.zone_id
   name    = "version.goauthentik.io"
