bb122326673c93262a82a2bca544eef4bf86e875
Jens Langhammer <jens.langhammer@beryju.org>
2022-10-23 14:32:02 +0200

    add k8s auth

--- tf/symbiosis/infra.tf
+++ tf/symbiosis/infra.tf
@@ -1,9 +1,9 @@
 provider "kubernetes" {
   host = "https://${symbiosis_cluster.infra.endpoint}"
 
-  client_certificate     = symbiosis_cluster.example.certificate
-  client_key             = symbiosis_cluster.example.private_key
-  cluster_ca_certificate = symbiosis_cluster.example.ca_certificate
+  client_certificate     = symbiosis_cluster.infra.certificate
+  client_key             = symbiosis_cluster.infra.private_key
+  cluster_ca_certificate = symbiosis_cluster.infra.ca_certificate
   alias                  = "beryjuio-infra"
 }
 
@@ -12,9 +12,16 @@ resource "symbiosis_cluster" "infra" {
   region = "germany-1"
 }
 
-resource "symbiosis_node_pool" "default" {
+resource "symbiosis_node_pool" "infra-default" {
   cluster   = symbiosis_cluster.infra.name
   name      = "default"
   node_type = "general-1"
   quantity  = 2
+}
+
+module "vault-infra" {
+  source = "../modules/vault_k8s"
+  path   = "k8s-${symbiosis_cluster.infra.name}"
+  host   = "https://${symbiosis_cluster.infra.endpoint}"
+  ca     = symbiosis_cluster.example.ca_certificate
 }
