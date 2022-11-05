ed9f10de4313275d14c41186d4527a36ec97764f
Jens Langhammer <jens.langhammer@beryju.org>
2022-11-05 16:57:11 +0100

    create cluster first

--- tf/symbiosis/prod.tf
+++ tf/symbiosis/prod.tf
@@ -1,32 +1,32 @@
-provider "kubernetes" {
-  alias = "beryjuio-prod"
+# provider "kubernetes" {
+#   alias = "beryjuio-prod"
 
-  host                   = "https://${symbiosis_cluster.prod.endpoint}"
-  client_certificate     = symbiosis_cluster.prod.certificate
-  client_key             = symbiosis_cluster.prod.private_key
-  cluster_ca_certificate = symbiosis_cluster.prod.ca_certificate
-}
+#   host                   = "https://${symbiosis_cluster.prod.endpoint}"
+#   client_certificate     = symbiosis_cluster.prod.certificate
+#   client_key             = symbiosis_cluster.prod.private_key
+#   cluster_ca_certificate = symbiosis_cluster.prod.ca_certificate
+# }
 
 resource "symbiosis_cluster" "prod" {
   name   = "beryjuio-prod"
   region = "germany-1"
 }
 
-data "kubernetes_secret_v1" "prod-vault-secret" {
-  provider = kubernetes.beryjuio-prod
-  metadata {
-    name      = "vault-secrets-operator"
-    namespace = "vault-secrets-operator"
-  }
-}
+# data "kubernetes_secret_v1" "prod-vault-secret" {
+#   provider = kubernetes.beryjuio-prod
+#   metadata {
+#     name      = "vault-secrets-operator"
+#     namespace = "vault-secrets-operator"
+#   }
+# }
 
-module "vault-prod" {
-  source = "../modules/vault_k8s"
-  path   = "k8s-${symbiosis_cluster.prod.name}"
-  host   = "https://${symbiosis_cluster.prod.endpoint}"
-  ca     = symbiosis_cluster.prod.ca_certificate
-  jwt    = data.kubernetes_secret_v1.prod-vault-secret.data["token"]
-}
+# module "vault-prod" {
+#   source = "../modules/vault_k8s"
+#   path   = "k8s-${symbiosis_cluster.prod.name}"
+#   host   = "https://${symbiosis_cluster.prod.endpoint}"
+#   ca     = symbiosis_cluster.prod.ca_certificate
+#   jwt    = data.kubernetes_secret_v1.prod-vault-secret.data["token"]
+# }
 
 resource "symbiosis_node_pool" "prod-default" {
   cluster   = symbiosis_cluster.prod.name
