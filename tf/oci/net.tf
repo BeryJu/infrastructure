167cdee12aada2eb9561da443f4fe8768f22fd80
Jens Langhammer <jens.langhammer@beryju.org>
2022-12-09 18:01:03 +0000

    use better network rules

--- tf/oci/net.tf
+++ tf/oci/net.tf
@@ -37,7 +37,8 @@ resource "oci_core_subnet" "homelab" {
 resource "oci_core_security_list" "orc_sl" {
   compartment_id = local.compartment_id
   vcn_id         = oci_core_vcn.orc.id
-  display_name   = "Default security list"
+  display_name   = "K8s Istio Security List"
+
   egress_security_rules {
     description      = ""
     protocol         = "all"
@@ -45,13 +46,106 @@ resource "oci_core_security_list" "orc_sl" {
     destination_type = "CIDR_BLOCK"
     stateless        = false
   }
+
   ingress_security_rules {
-    description = ""
-    protocol    = "all"
+    description = "SSH"
+    protocol    = 6
     source      = "0.0.0.0/0"
+    tcp_options {
+      min = 22
+      max = 22
+    }
     source_type = "CIDR_BLOCK"
     stateless   = true
   }
+
+  ingress_security_rules {
+    description = "k8s-api"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 6443
+      max = 6443
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+
+  ingress_security_rules {
+    description = "http"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 80
+      max = 80
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+  ingress_security_rules {
+    description = "https"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 443
+      max = 443
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+
+  ingress_security_rules {
+    description = "istio-status"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 15021
+      max = 15021
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+  ingress_security_rules {
+    description = "istio-tls"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 15443
+      max = 15443
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+  ingress_security_rules {
+    description = "istio-tls-istiod"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 15012
+      max = 15012
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+  ingress_security_rules {
+    description = "istio-tls-webhook"
+    protocol    = 6
+    source      = "0.0.0.0/0"
+    tcp_options {
+      min = 15017
+      max = 15017
+    }
+    source_type = "CIDR_BLOCK"
+    stateless   = true
+  }
+
+
   ingress_security_rules {
     description = ""
     protocol    = 1
@@ -63,6 +157,7 @@ resource "oci_core_security_list" "orc_sl" {
       code = "4"
     }
   }
+
   ingress_security_rules {
     description = ""
     protocol    = 1
