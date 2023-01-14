eacdd9bffa683991d8317743856e8b70e6120e88
Jens Langhammer <jens.langhammer@beryju.org>
2023-01-14 19:22:35 +0100

    use proxy for now

--- tf/authentik/k8s-mimir.tf
+++ tf/authentik/k8s-mimir.tf
@@ -3,8 +3,8 @@ module "authentik-app-mimir" {
 
   name     = "Mimir"
   group    = "Monitoring"
-  internal = ""
-  external = "https://mimir.beryjuio-prod.k8s.beryju.org/"
+  internal = "http://mimir.mimir:9000"
+  external = "https://mimir.beryjuorg-prd.k8s.beryju.org"
   access_group = [
     data.authentik_group.acl_beryjuorg.id
   ]
