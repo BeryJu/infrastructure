addc0a54e8c36db03cb9e56d1262f720bb1fb421
Jens Langhammer <jens.langhammer@beryju.org>
2023-02-19 23:25:06 +0100

    idk

--- tf/vault/ssh.tf
+++ tf/vault/ssh.tf
@@ -2,3 +2,18 @@ resource "vault_mount" "ssh-client-signer" {
   type = "ssh"
   path = "ssh-client-signer"
 }
+
+resource "vault_ssh_secret_backend_role" "jens" {
+  name                    = "jens"
+  backend                 = vault_mount.ssh-client-signer.path
+  key_type                = "ca"
+  algorithm_signer        = "rsa-sha2-256"
+  allow_user_certificates = true
+  allowed_users           = "*"
+  allowed_extensions      = "permit-pty,permit-port-forwarding"
+  default_extensions = {
+    "permit-pty" : ""
+  }
+  default_user = "jens"
+  ttl          = "30m0s"
+}
