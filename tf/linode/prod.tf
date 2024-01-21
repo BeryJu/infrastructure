87ca2bb2a77005b2ffbf03a07c0ec5637268d5bf
Jens Langhammer <jens.langhammer@beryju.org>
2024-01-21 21:53:42 +0100

    fix linode k8s version

--- tf/linode/prod.tf
+++ tf/linode/prod.tf
@@ -1,6 +1,6 @@
 resource "linode_lke_cluster" "prod" {
   label       = "beryjuio-prod"
-  k8s_version = "1.27"
+  k8s_version = "1.28"
   region      = "eu-central"
   control_plane {
     high_availability = false
