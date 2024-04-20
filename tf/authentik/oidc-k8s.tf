12f2d8fa6ca6f9ed6e6ee32e6fe7d8846e6e9c3d
Jens Langhammer <jens.langhammer@beryju.org>
2024-04-20 15:20:48 +0200

    add home cluster

--- tf/authentik/oidc-k8s.tf
+++ tf/authentik/oidc-k8s.tf
@@ -1,3 +1,11 @@
+module "authentik-k8s-oidc-home" {
+  source = "../modules/authentik_k8s_oidc/"
+  name   = "beryjuio-home"
+  access_group = [
+    data.authentik_group.acl_beryjuorg.id
+  ]
+}
+
 module "authentik-k8s-oidc-oci" {
   source = "../modules/authentik_k8s_oidc/"
   name   = "beryjuio-oci"
