666b457e4f4a5425adce7e0b0d85a6746343d73f
Jens Langhammer <jens.langhammer@beryju.org>
2025-09-05 21:54:20 +0200

    poetry -> uv

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARA_API_CLIENT=http
 export ARA_API_SERVER="https://ara.beryjuio-home.k8s.beryju.io"
-export ANSIBLE_CALLBACK_PLUGINS=$(poetry run python3 -m ara.setup.callback_plugins)
+export ANSIBLE_CALLBACK_PLUGINS=$(uv run python3 -m ara.setup.callback_plugins)
 ansible-playbook --diff $@
