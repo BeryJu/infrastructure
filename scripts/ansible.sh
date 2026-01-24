1cdcbefc9c0223a73d68ce41458502e1f0461c48
Jens Langhammer <jens.langhammer@beryju.org>
2026-01-24 23:39:36 +0100

    split kube and containerd role, add dev container

--- scripts/ansible.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export ARA_API_CLIENT=http
-export ARA_API_SERVER="https://ara.beryjuio-home.k8s.beryju.io"
-export ANSIBLE_CALLBACK_PLUGINS=$(uv run python3 -m ara.setup.callback_plugins)
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-ansible-playbook --diff $@
